b90906fd1b3d641be7f7474baa8df9615cea12ed
Dayhawk <ayushchandwani26@gmail.com>
2023-10-28 13:16:53 +0530

    implemented webview

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, PermissionsAndroid } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
+const App = () => {
+  // Function to request camera and microphone permissions
+  const requestPermissions = async () => {
+    try {
+      const cameraPermission = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA
+      );
+      const microphonePermission = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
+      );
+
+      if (
+        cameraPermission === PermissionsAndroid.RESULTS.GRANTED &&
+        microphonePermission === PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        console.log('Camera and microphone permissions granted.');
+      } else {
+        console.log('Camera and microphone permissions denied.');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  React.useEffect(() => {
+    // Request camera and microphone permissions when the component mounts
+    requestPermissions();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        source={{ uri: 'https://ar.gofactz.com/' }} // Replace with your desired URL
+        allowsFullscreenVideo={true} // Enable full-screen video support
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
